dba018a5ec4f644dd8a49e811163b8dd4bb369ec
Tirnovan Camelia <c.tirnovan@viamedici.de>
2026-04-03 13:19:29 +0300

    code refactored

--- src/components/filters/HorizontalFilters.tsx
+++ src/components/filters/HorizontalFilters.tsx
@@ -3,6 +3,7 @@ import { Badge, Button, Flex } from "antd";
 import { useSetAtom } from "jotai";
 import { isOpenFilterDrawerAtom, selectedFilterAtom } from "../../storageAtoms";
 import { useSearchParams } from "react-router-dom";
+import { filters } from "./filtersData";
 
 const HorizontalFilters: React.FC = () => {
   const setSelectedFilter = useSetAtom(selectedFilterAtom);
@@ -15,58 +16,29 @@ const HorizontalFilters: React.FC = () => {
       className="sticky z-900 bg-white-bg"
       style={{ top: "137px" }}
     >
-      <div className="hidden sm:flex pb-2 mb-2 gap-4 overflow-x-auto whitespace-nowrap lg:max-w-full min-[880px]:max-w-full min-[768px]:max-w-md min-[640px]:max-w-sm min-[460px]:max-w-124 max-w-64">
-        <Button
-          onClick={() => {
-            setIsOpenFilterDrawer(true);
-            setSelectedFilter({ key: "style", name: "Stil" });
-          }}
-        >
-          In stoc
-          <Badge count={searchParams.getAll("stock")?.length} color="#000" />
-        </Button>
-        <Button
-          onClick={() => {
-            setIsOpenFilterDrawer(true);
-            setSelectedFilter({ key: "color", name: "Culoare" });
-          }}
-        >
-          Culoare
-          <Badge count={searchParams.getAll("color")?.length} color="#000" />
-        </Button>
-        <Button
-          onClick={() => {
-            setIsOpenFilterDrawer(true);
-            setSelectedFilter({ key: "size", name: "Dimensiune" });
-          }}
-        >
-          Marimea posetei
-          <Badge count={searchParams.getAll("size")?.length} color="#000" />
-        </Button>
-        <Button>Material</Button>
-        <Button
-          onClick={() => {
-            setIsOpenFilterDrawer(true);
-            setSelectedFilter({
-              key: "handle",
-              name: "Tipul manerului gentii",
-            });
-          }}
-        >
-          Tipul manerului gentii
-          <Badge count={searchParams.getAll("handle")?.length} color="#000" />
-        </Button>
-        <Button
-          onClick={() => {
-            setIsOpenFilterDrawer(true);
-            setSelectedFilter({ key: "style", name: "Stil" });
-          }}
-        >
-          Stilul gentii
-          <Badge count={searchParams.getAll("style")?.length} color="#000" />
-        </Button>
+      {/* // <Row
+    //   gutter={16}
+    //   className="sticky z-900 bg-white-bg w-full overflow-x-auto"
+    //   style={{ top: "137px" }}
+    //   wrap={false}
+    // > */}
+      <div className="hidden sm:flex pb-2 mb-2 gap-4 overflow-x-auto whitespace-nowrap lg:max-w-full min-[880px]:max-w-full min-[768px]:max-w-9/10 min-[640px]:max-w-sm min-[460px]:max-w-124 max-w-64">
+        {filters.map(({ key, name }) => (
+          // <Col className="mb-2 pb-2">
+          <Button
+            onClick={() => {
+              setIsOpenFilterDrawer(true);
+              setSelectedFilter({ key: key, name: name });
+            }}
+          >
+            {name}
+            <Badge count={searchParams.getAll(key)?.length} color="#000" />
+          </Button>
+          // </Col>
+        ))}
       </div>
     </Flex>
+    // </Row>
   );
 };
 
