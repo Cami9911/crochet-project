5b5475d4a68c06ad9f3db27a1a1257083d368c26
Tirnovan Camelia <c.tirnovan@viamedici.de>
2026-04-20 10:44:25 +0300

    contact page updated

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,14 +4,36 @@ import ContactData from "./contact/ContactData";
 
 const Contact = () => {
   return (
-    <Row className=" px-4 sm:px-2 md:px-12 xl:px-40">
-      <Col span={17}>
-        <ContactForm />
-      </Col>
-      <Col span={7}>
-        <ContactData />
-      </Col>
-    </Row>
+    <>
+      <Row className="flex justify-center items-center bg-[#a06518] h-48 text-white ">
+        <p className="text-[40px]">
+          Contacteaza-ma pentru mai multe informatii
+        </p>
+      </Row>
+      <div className=" px-4 sm:px-2 md:px-12 xl:px-40">
+        <Row className="mt-8 mb-12">
+          <span>
+            Daca unul dintre produsele mele ti-a facut cu ochiul, nu ezita sa ma
+            contactezi! Poate ti-a placut culoarea unui produs, dar modelul
+            altuia, sau poate doresti alte accesorii, nu este nicio problema.
+            Contacteaza-ma si vedem ce putem rezolva!
+          </span>
+          <span>
+            Mai jos gasesti un formular pentru a-mi trimite un mail sau si mai
+            usor, paginile mele de social media (Instagram si Facebook) unde imi
+            poti scrie direct.
+          </span>
+        </Row>
+        <Row gutter={[16, 16]}>
+          <Col xs={24} md={11}>
+            <ContactForm />
+          </Col>
+          <Col xs={24} md={{ span: 9, offset: 4 }}>
+            <ContactData />
+          </Col>
+        </Row>
+      </div>
+    </>
   );
 };
 
--- src/pages/contact/ContactForm.tsx
+++ src/pages/contact/ContactForm.tsx
@@ -18,9 +18,8 @@ const ContactForm = () => {
   return (
     <Form
       name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      //   style={{ maxWidth: 600 }}
+      // wrapperCol={{ span: 18 }}
+      // style={{ width: "100%" }}
       initialValues={{ remember: true }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
