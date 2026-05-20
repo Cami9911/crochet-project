28f348b507cf9c04550d9ef83314c66ecd056163
Tirnovan Camelia <c.tirnovan@viamedici.de>
2026-05-20 10:33:52 +0300

    new products added

--- src/useFunctions.tsx
+++ src/useFunctions.tsx
@@ -1,2 +1,4 @@
-export const capitalizeFirst = (text: string) =>
-  text.charAt(0).toUpperCase() + text.slice(1);
+export const capitalizeFirst = (text: string) => {
+  if (!text) return "";
+  return text.charAt(0).toUpperCase() + text.slice(1);
+};
